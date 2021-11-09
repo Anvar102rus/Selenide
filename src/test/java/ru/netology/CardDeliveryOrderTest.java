83e5f3dbe23108329943447a9fd549773e507fa9
AnvarPro <anvarvahitov30@rambler.com>
2021-11-09 22:29:50 +0500

    buildGradle.CDOTest

--- src/test/java/ru/netology/CardDeliveryOrderTest.java
+++ src/test/java/ru/netology/CardDeliveryOrderTest.java
@@ -1,34 +1,42 @@
 package ru.netology;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.testng.annotations.Test;
-
-import static com.codeborne.selenide.Condition.visible;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Selectors.withText;
-import static com.codeborne.selenide.Selenide.*;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 
-public class CardDeliveryOrder {
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardDeliveryOrderTest {
     public String getDate(int days, String formattedDate) {
         return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern(formattedDate));
     }
 
     @Test
     void shouldCheckRegistrationForm() {
-        String DateOfMeeting = getDate(3, "dd.MM.yyyy");
+        String dateOfMeeting = getDate(3, "dd.MM.yyyy");
 
         open("http://localhost:9999/");
         $("[data-test-id=city] input").setValue("Уфа");
         $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(DateOfMeeting);
+        $("[data-test-id=date] input").setValue(dateOfMeeting);
         $("[data-test-id=name] input").setValue("Анвар");
         $("[data-test-id=phone] input").setValue("+79999999999");
         $("[data-test-id=agreement]").click();
         $(By.className("button")).click();
         $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(20));
+        $(".notification__content")
+                .shouldBe(visible, Duration.ofSeconds(20))
+                .shouldHave(exactText("Встреча успешно забронирована на " + dateOfMeeting));
+
     }
 }
+
